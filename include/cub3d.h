bb9f7e53ac2e7b96597433a708d0cf63ed74ad69
TwindZ <emmanuel.lamothe@gmail.com>
2023-09-07 15:01:23 -0400

    une image !!!!

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,22 +6,83 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 17:00:29 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/09/07 09:43:26 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/09/07 14:38:09 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+//----------define
+# define WINWIDTH 2048
+# define WINHEIGHT 1280
+
+//----------//----------error message
+#define ERRARGC "Usage : ./cub3D <map.cub>\n"
+
+//----------//----------temp
+# define TEMPMAP "./graphics/bellebriquewow.png"
+
 //----------include
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
+# include "../srcs/libft/libft.h"
 # include "../srcs/MLX42/include/MLX42/MLX42.h"
 
+//----------struct
+typedef struct s_flood
+{
+	char			**map;
+	int				collect;
+	int				exit;
+	int				done;
+}			t_flood;
 
+typedef struct s_player
+{
+	int				pos_x;
+	int				pos_y;
+}			t_player;
 
+typedef struct s_base_param
+{
+	char			*north;
+	char			*south;
+	char			*east;
+	char			*west;
+	char			*cell;
+	char			*floor;
+}			t_base_param;
 
+typedef struct s_image
+{
+	mlx_image_t		*north;
+	mlx_image_t		*south;
+	mlx_image_t		*east;
+	mlx_image_t		*west;
+	mlx_image_t		*cell;
+	mlx_image_t		*floor;
+}			t_image;
+
+typedef struct s_texture
+{
+	mlx_texture_t	*nord;
+	mlx_texture_t	*south;
+	mlx_texture_t	*east;
+	mlx_texture_t	*west;
+	mlx_texture_t	*cell;
+	mlx_texture_t	*floor;
+}			t_texture;
+
+typedef struct s_data
+{
+	mlx_t			*mlx;
+	t_base_param	visual;
+	t_texture		texture;
+	t_image			image;
+	t_image			old_image;
+}			t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,37 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 16:57:57 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/09/07 09:56:42 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/09/07 15:01:01 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int main()
+int	errhandler(char *msg)
 {
-	puts("salu");
+	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd(msg, 2);
+	return(-1);
+}
+
+t_data	*init_data(t_data *data, char **argv)
+{
+	if(!argv)
+		return(data);
+	return(data);
+}
+
+int main(int argc, char **argv)
+{
+	t_data data;
+	
+	if(argc != 2)
+		return(errhandler(ERRARGC));
+	init_data(&data, argv);
+	data.mlx = mlx_init(WINWIDTH, WINHEIGHT, "cub3D", 0);
+	data.texture.east = mlx_load_png(TEMPMAP);
+	data.image.east = mlx_texture_to_image(data.mlx, data.texture.east);
+	mlx_image_to_window(data.mlx, data.image.east, 0, 0);
+	mlx_loop(data.mlx);
+	
 }
