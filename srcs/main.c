981868c68171f3fd9d95b3f137f92f6f70c6a646
TwindZ <emmanuel.lamothe@gmail.com>
2023-09-21 09:57:53 -0400

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 16:57:57 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/09/20 18:52:39 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/09/21 09:44:19 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,22 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 	data = param;
 	if(mlx_is_key_down(data->mlx, MLX_KEY_W))
 	{
-		data->image.miniplayer->instances->y -= 2.5;
+		data->image.miniplayer->instances[0].y -= 2.5;
 		// ft_printf("w");
 	}
 	if(mlx_is_key_down(data->mlx, MLX_KEY_A))
 	{
-		data->image.miniplayer->instances->x -= 2.5;
+		data->image.miniplayer->instances[0].x -= 2.5;
 		// ft_printf("a");
 	}
 	if(mlx_is_key_down(data->mlx, MLX_KEY_S))
 	{
-		data->image.miniplayer->instances->y += 2.5;
+		data->image.miniplayer->instances[0].y += 2.5;
 		// ft_printf("s");
 	}
 	if(mlx_is_key_down(data->mlx, MLX_KEY_D))
 	{
-		data->image.miniplayer->instances->x += 2.5;
+		data->image.miniplayer->instances[0].x += 2.5;
 		// ft_printf("d");
 	}
 	if(mlx_is_key_down(data->mlx, MLX_KEY_RIGHT))
@@ -155,33 +155,13 @@ void	ft_img_to_win(t_data *data)
 	mlx_image_to_window(data->mlx, data->image.miniplayer, data->player.pos_x , data->player.pos_y - (data->player.start_map * MINITILES));
 }
 
-void	clear_img(mlx_image_t *image, int width, int height)
-{
-	int width_reset;
-
-	width_reset = width;
-	while(height)
-	{
-		width = width_reset;
-		while(width)
-		{
-			mlx_put_pixel(image, width - 1, height - 1, 0);
-			width--;
-		}
-		height--;
-	}
-}
-
 void	render(void *param)
 {
 	t_data *data;
 	data = param;
 	(void) data;
-	// mlx_delete_image(data->mlx, data->image.minifloor);
-	// mlx_delete_image(data->mlx, data->image.miniwall);
 	mlx_delete_image(data->mlx, data->image.window);
 	data->image.window = mlx_new_image(data->mlx, WINWIDTH, WINHEIGHT);
-	// clear_img(data->image.window, WINWIDTH, WINHEIGHT);
 	dda_algorithm(data->image.miniplayer->instances->x + (MINITILES / 4), data->image.miniplayer->instances->y + (MINITILES / 4), data->angle, data->image.window);
 	mlx_image_to_window(data->mlx, data->image.window, 0, 0);
 	printf("W %d\n", data->image.miniwall->count);
